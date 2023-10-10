4014bd2acd980426f22047aaeb6b1806ce941f98
Aye Myat Mon <mon@Ayes-MacBook-Pro.local>
2023-10-10 18:05:45 +0800

    Script

--- pages/index.js
+++ pages/index.js
@@ -4,22 +4,37 @@ import Work_experience from "../components/work_experience";
 import Skills from "../components/skills";
 import Projects from "../components/projects";
 import Get_in_touch from "../components/get_in_touch";
+import Script from "next/script";
 
 export default function Home() {
   // const posts = data.posts;
   // console.log(posts);
 
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <Work_experience />
-      <Skills />
-      <Projects />
-      <Get_in_touch />
-
-      {/* <Link href="posts/first-post">Post Page</Link> */}
-    </Layout>
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-QKZ7FCCQ1R"
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-QKZ7FCCQ1R');
+        `}
+      </Script>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <Work_experience />
+        <Skills />
+        <Projects />
+        <Get_in_touch />
+        {/* <Link href="posts/first-post">Post Page</Link> */}
+      </Layout>
+    </>
   );
 }
