7d30e6e5a861e13dc80e08910cc8a2dbe03edcaf
Myat Mon <ayemyatmon.yn@gmail.com>
2025-03-25 16:01:36 +0000

    style changes

--- components/button.js
+++ components/button.js
@@ -1,8 +1,11 @@
 import { BsArrowRight } from "react-icons/bs";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Button({ children }) {
   return (
-    <button className="flex items-center bg-emerald-300 text-black py-1 px-2 rounded hover:bg-black hover:text-white">
+    <button
+      className={`${utilStyles.has_hover_effect}  flex items-center bg-emerald-300 font-medium text-black py-2 px-2 rounded hover:bg-black hover:text-white`}
+    >
       {children} <BsArrowRight className="ml-2" />
     </button>
   );
