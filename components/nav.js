307eddf791f9af7274e88c9ce9e4c750586f13a9
Myat Mon <ayemyatmon.yn@gmail.com>
2025-05-28 14:09:03 +0100

    skin changes

--- components/nav.js
+++ components/nav.js
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function TopNav() {
+  const router = useRouter();
+  const isActive = (pathname) => router.pathname === pathname;
+
   return (
     <nav className="bg-blue-600 text-white p-3 w-full fixed z-10">
       <ul className="flex justify-center items-center space-x-8 md:space-x-20 text-sm font-medium">
         <li>
           <Link
             href="/"
-            className="text-white hover:text-blue-100 hover:border-b-2 hover:border-blue-100"
+            className={`text-white hover:text-blue-100 hover:border-b-2 hover:border-blue-100 ${
+              isActive("/") ? "border-b-2 border-blue-200" : ""
+            }`}
           >
             Home
           </Link>
@@ -15,7 +21,9 @@ export default function TopNav() {
         <li>
           <Link
             href="/about"
-            className="text-white hover:text-blue-100 hover:border-b-2 hover:border-blue-100"
+            className={`text-white hover:text-blue-100 hover:border-b-2 hover:border-blue-100 ${
+              isActive("/about") ? "border-b-2 border-blue-200" : ""
+            }`}
           >
             About Me
           </Link>
@@ -23,7 +31,9 @@ export default function TopNav() {
         <li>
           <Link
             href="/projects"
-            className="text-white hover:text-blue-100 hover:border-b-2 hover:border-blue-100"
+            className={`text-white hover:text-blue-100 hover:border-b-2 hover:border-blue-100 ${
+              isActive("/projects") ? "border-b-2 border-blue-200" : ""
+            }`}
           >
             Projects
           </Link>
@@ -31,7 +41,9 @@ export default function TopNav() {
         <li>
           <Link
             href="/contact"
-            className="text-white hover:text-blue-100 hover:border-b-2 hover:border-blue-100"
+            className={`text-white hover:text-blue-100 hover:border-b-2 hover:border-blue-100 ${
+              isActive("/contact") ? "border-b-2 border-blue-200" : ""
+            }`}
           >
             Contact
           </Link>
