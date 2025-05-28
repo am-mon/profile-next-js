5c6ab9444228a2b2cd55a8a6c57d7b9aac5a9334
Myat Mon <ayemyatmon.yn@gmail.com>
2025-05-28 14:49:16 +0100

    skin updates

--- components/footer.js
+++ components/footer.js
@@ -4,6 +4,9 @@ export default function Footer(params) {
     <footer className="w-full text-center py-6 bg-blue-600 text-white">
       <p className="text-sm text-white">
         © {new Date().getFullYear()} Mon. All rights reserved.
+        <span className="hidden md:inline-block">&nbsp;|&nbsp;</span>
+        <br className="inline-block md:hidden" />
+        Website developed using <i>Next.js</i>
       </p>
     </footer>
   );
